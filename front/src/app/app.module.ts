eedc4fb38be1ac29a00ed797d047d6003d2fdcfd
Marina Timoshenko <nemascha@gmail.com>
2018-01-18 22:51:59 +0300

    add angular material

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
 
 
 import { AppComponent } from './app.component';
+import { EnterResultsComponent } from './enter-results/enter-results.component';
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EnterResultsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
