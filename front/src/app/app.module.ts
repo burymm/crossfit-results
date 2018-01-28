817ef5e2f88249bc8985fa0e0be9321ba914e8be
Marina Timoshenko <nemascha@gmail.com>
2018-01-28 13:00:56 +0300

    add dark-theme

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
-
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { EnterResultsComponent } from './enter-results/enter-results.component';
@@ -21,8 +22,12 @@ import { EnterResultsComponent } from './enter-results/enter-results.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
     MatDatepickerModule,
-    MatInputModule
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
