41e55da03653c2c28c0ec30c646d2ccee80ec198
nemascha <nemascha@gmail.com>
2018-08-22 14:57:46 +0300

    ref table add link for cardNumber

--- front/src/app/view-results/view-results.component.ts
+++ front/src/app/view-results/view-results.component.ts
@@ -7,6 +7,8 @@ import {EnterResultsComponent} from "../enter-results/enter-results.component";
 import {ExerciseService} from "../services/exercise.service";
 import {Exercise} from "../models/models";
 import { ResultsService } from '../services/results.service';
+import {CellClickedEvent} from "ag-grid";
+import {a} from "@angular/core/src/render3";
 
 
 @Component({
@@ -20,7 +22,9 @@ export class ViewResultsComponent implements OnInit {
 
   columnDefs = [
     {headerName: 'Date', field: 'trainingDate', minWidth: 50},
-    {headerName: 'Card Number', field: 'cardNumber', minWidth: 50 },
+    {headerName: 'Card Number', field: 'cardNumber', minWidth: 50, cellRenderer: function (params) {
+      return '<a href="/results/:cardNumber" style="text-decoration: none; color: white;">'+ params.value+'</a>';
+    }},
     {headerName: 'Exercise', field: 'exerciseName', minWidth: 50},
     {headerName: 'Result', field: 'workoutResult', minWidth: 50}
   ];
@@ -52,13 +56,13 @@ export class ViewResultsComponent implements OnInit {
     });
   }
 
-  showUserResult(cardNumber: string) {
-    if (!cardNumber) {
-      return;
-    }
-
-    this.router.navigate([`/results/`, cardNumber]);
-  }
+  // showUserResult(cardNumber: string) {
+  //   if (!cardNumber) {
+  //     return;
+  //   }
+  //
+  //   this.router.navigate([`/results/`, cardNumber]);
+  // }
 
   private loadResults() {
     return this.rest.getResults().subscribe((results) => {
@@ -79,5 +83,6 @@ export class ViewResultsComponent implements OnInit {
       return result;
     });
   }
+
 
 }
